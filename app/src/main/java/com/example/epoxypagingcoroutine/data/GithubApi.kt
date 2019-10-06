a41129d77629f11dc63c44a1399918e7ec6579a3
ho2ri <ho2ri2s.github@gmail.com>
2019-10-06 12:50:48 +0900

    fix: modify parameter of api function

--- app/src/main/java/com/example/epoxypagingcoroutine/data/GithubApi.kt
+++ app/src/main/java/com/example/epoxypagingcoroutine/data/GithubApi.kt
@@ -4,9 +4,13 @@ import com.example.epoxypagingcoroutine.data.model.Repo
 import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Path
+import retrofit2.http.Query
 
 interface GithubApi {
 
-    @GET("/users/{username}/repos")
-    suspend fun getGithubRepos(@Path("username") username: String): Response<List<Repo>>
+    @GET("/users/{user}/repos")
+    suspend fun getGithubRepos(
+        @Path("user") username: String,
+        @Query("page") page: String,
+        @Query("per_page") perPage: String): Response<List<Repo>>
 }
